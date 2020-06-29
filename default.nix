f427371023705bed3b9c871cbde22d795f11315b
hhefesto <hhefesto@rdataa.com>
2020-06-28 20:52:18 -0500

    initial commit

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ # Fetch the latest haskell.nix and import its default.nix
+  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
+  # Pinned version gives error "no source atribute".
+  # haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/f1a94a4c82a2ab999a67c3b84269da78d89f0075.tar.gz") {}
+# haskell.nix provides access to the nixpkgs pins which are used by our CI,
+# hence you will be more likely to get cache hits when using these.
+# But you can also just use your own, e.g. '<nixpkgs>'.
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+
+# haskell.nix provides some arguments to be passed to nixpkgs, including some
+# patches and also the haskell.nix functionality itself as an overlay.
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+
+# import nixpkgs with overlays
+, pkgs ? import nixpkgsSrc nixpkgsArgs
+}: pkgs.haskell-nix.project {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "rrumikub";
+    src = ./.;
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,14 @@
+# shell.nix
+let
+  hsPkgs = import ./default.nix {};
+in
+  hsPkgs.my-package.components.all
+
+# # shell.nix
+# let
+#   haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {};
+#   nixpkgs = import haskellNix.sources.nixpkgs-1909 haskellNix.nixpkgsArgs;
+#   haskell = nixpkgs.haskell-nix;
+# in
+#   haskell.haskellPackages.ghcWithPackages (ps: with ps;
+#     [ lens conduit conduit-extra ])
